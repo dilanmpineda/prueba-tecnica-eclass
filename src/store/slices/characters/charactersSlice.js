3573b472df934ba4c469b603d3b94c7fd9eeba73
Dilan Martinez <dilanmpineda@gmail.com>
2022-08-09 23:53:40 -0400

    correccion de carga de favoritos al recargar pagina

--- src/store/slices/characters/charactersSlice.js
+++ src/store/slices/characters/charactersSlice.js
@@ -3,14 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 export const favCharactersSlice = createSlice({
     name: 'favCharacters',
     initialState: {
-        favorites: localStorage.getItem('favorites') != null ? localStorage.getItem('favorites') : []
+        favorites: localStorage.getItem('favorites') != null ? localStorage.getItem('favorites').trim().split(',') : []
     },
     reducers: {
         addCharacter: (state, action) => {
+            console.log(localStorage.getItem('favorites'));
             let characterId = action.payload.characterId;
             let inFavorites = action.payload.inFavorites;
             if (inFavorites) {
-                let favoritesToUpdate = localStorage.getItem('favorites').split(',');
+                let favoritesToUpdate = localStorage.getItem('favorites').trim().split(',');
                 let index = favoritesToUpdate.indexOf(characterId);
                 favoritesToUpdate.splice(index, 1);
                 state.favorites = [...favoritesToUpdate];
